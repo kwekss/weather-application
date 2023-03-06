f186ca110f87941425cabfc30223c4b7ad5c150a
JohnAndrewsAsare <asarejohnandrews@gmail.com>
2023-03-06 01:08:54 +0000

    weather app

---
+++ components/Spinner.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+import spinner from "../public/spinner.gif";
+
+const Spinner = () => {
+  return (
+    <div>
+      <Image
+        className="w-[200px] m-auto block"
+        src={spinner}
+        alt="loading..."
+      />
+    </div>
+  );
+};
+
+export default Spinner;
